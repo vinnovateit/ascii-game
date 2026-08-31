b3237b22eae7414e4ffbf32618335c9e303af461
Ameya <ameyakakade@gmail.com>
2026-09-01 02:41:31 +0530

    Hot reloading does work but is unfinished.
    Everything is being done in the main function for now. Moving code to
    functions is difficult because of lifetime problems.

--- src/bin/hot.rs
+++ src/bin/hot.rs
@@ -1,32 +1,27 @@
 // code for hot reloading bin.
 use ascii_game::init::*;
 use ascii_game::state::*;
+use libloading::*;
 
 fn main() {
     let mut state = init_state();
     println!("Hot reloading is a work in progress!");
     unsafe {
-        let mut lib: libloading::Library;
-        let mut fun: libloading::Symbol<unsafe fn(*mut State) -> State>;
-        (lib, fun) = load();
+        let mut lib = match Library::new("target/debug/libascii_game.so"){
+            Ok(i) => i,
+            Err(_) => panic!("Could not load library.")
+        };
         loop {
-            fun = reload(&mut lib);
+            Library::close(lib);
+            lib = match Library::new("target/debug/libascii_game.so"){
+                Ok(i) => i,
+                Err(_) => panic!("Could not load library.")
+            };
+            let mut fun: Symbol<unsafe fn(*mut State) -> State> = match lib.get(b"fmain"){
+                Ok(i) => i,
+                Err(_) => panic!("Could not load `fmain` fn.")
+            };
             fun(&mut state);
         }
-    }
-}
-
-unsafe fn load<'a>() -> (libloading::Library, libloading::Symbol<'a, unsafe fn(*mut State) -> State>) {
-    todo!();
-}
-
-unsafe fn reload(lib: &mut libloading::Library) -> libloading::Symbol<unsafe fn(*mut State) -> State> {
-    *lib = match libloading::Library::new("target/debug/libascii_game.so"){
-        Ok(i) => i,
-        Err(_) => panic!("Could not load library.")
-    };
-    match lib.get(b"fmain"){
-        Ok(i) => i,
-        Err(_) => panic!("Could not load `fmain` fn.")
     }
 }
--- src/game_loop.rs
+++ src/game_loop.rs
@@ -4,7 +4,7 @@ use crate::state::*;
 
 #[unsafe(no_mangle)]
 pub fn fmain(state: &mut State) {
-    println!("This is the main game loop: {}", state.counter);
+    println!("This is game logic: {}", state.counter);
     state.counter += 1;
     // sleeping to simulate work.
     // this will be replaced by game logic that runs on each frame.
